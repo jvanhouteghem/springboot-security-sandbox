1a877650079339c66bbff56b7fc80baa921c2396
jvanhouteghem <vanhouteghem.j@gmail.com>
2017-06-21 23:08:32 +0200

    add custom admin and user

--- springsecuritysandbox/src/main/java/com/example/demo/SecurityConfig.java
+++ springsecuritysandbox/src/main/java/com/example/demo/SecurityConfig.java
@@ -1,11 +1,26 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableGlobalMethodSecurity(securedEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	@Autowired
+	public void configureAuth(AuthenticationManagerBuilder auth) throws Exception {
+		auth
+			.inMemoryAuthentication()
+				.withUser("admin")
+				.password("admin")
+				.roles("ADMIN")
+			.and()
+				.withUser("user")
+				.password("user")
+				.roles("USER");
+	}
 
 }
